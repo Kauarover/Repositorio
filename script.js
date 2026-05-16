93eac9e916269445fccb2af138ed9e338a0369db
Kauarover <159644466+Kauarover@users.noreply.github.com>
2026-05-16 18:23:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Evento para o botão "Sim"
+document.getElementById("yes").addEventListener("click", function() {
+    window.location.href = "index2.html";
+});
+
+// Evento para o botão "Não"
+document.getElementById("no").addEventListener("click", function() {
+    alert("Não existe essa opção!");
+});
